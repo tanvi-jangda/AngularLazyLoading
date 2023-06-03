f9f9c26b679be3cea925dc9042eeeec876ce1334
tanvi-jangda <tanvi.jangda@gmail.com>
2023-06-03 22:17:43 +0530

    initial commin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+/*const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: './admin/admin.module#AdminModule'
+  },
+  {
+    path: 'staff',
+    loadChildren: './staff/staff.module#StaffModule'
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];*/
+
+const routes:Routes = [
+  {path : 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+  {path: 'staff', loadChildren: () => import('./staff/staff.module').then(m => m.StaffModule) },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
